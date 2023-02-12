b32aceb9a90b07ba73c3cfa343fa41c31d3993ce
Jiafang Song <jsong@compute-124.cm.cluster>
2023-02-12 16:43:52 -0500

    completed assignment hw1

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -5,4 +5,7 @@ do
   let linenum="10+$commit_count"
   # Fill in
   sed -i "$linenum i This is the sentence number $commit_count." foo.tex
+  pdflatex foo.tex
+  git add -u
+  git commit -m "Add sentence number $commit_count to TeX file"
 done
