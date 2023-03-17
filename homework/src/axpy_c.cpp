285c19d2bc8110715e4f120213a74fab2b9cedf8
jfsong100 <jsong100@jhu.edu>
2023-03-17 16:13:18 -0400

    complete hw4

--- homework/src/axpy_c.cpp
+++ homework/src/axpy_c.cpp
@@ -3,6 +3,12 @@ using Rcpp::NumericVector;
 
 // [[Rcpp::export]]
 NumericVector axpy_c(double a, NumericVector& x, NumericVector& y) {
+  if (x.size() > y.size()) {
+    Rcpp::stop("Incompatible vector lengths. x has a larger size");
+  }
+  if (x.size() < y.size()) {
+    Rcpp::warning("x has a smaller size");
+  }
   int n = x.size();
   for (int i = 0; i < n; ++i) {
     y[i] += a * x[i];
--- homework/src/matmat.cpp
+++ homework/src/matmat.cpp
@@ -14,6 +14,14 @@ NumericMatrix row_col_dot_matmat(
   int n_col_out = B.ncol();
   NumericMatrix result(n_row_out, n_col_out);
   // Fill in; remember to use (,) instead of [,] for accessing matrix elements
+  for (int i = 0; i < n_row_out; ++i) {
+    for (int j = 0; j < n_col_out; ++j){
+      result(i, j) = 0;
+      for (int q = 0; q < A.ncol(); ++q){
+        result(i, j) += A(i, q) * B(q, j);
+      }
+    }
+  }
   return result;
 }
 
@@ -25,11 +33,18 @@ NumericMatrix col_oriented_matmat(
     Rcpp::stop("Incompatible dimensions");
   }
   int n_row_out = A.nrow();
+  int n_row_A = A.ncol();
   int n_col_out = B.ncol();
   NumericMatrix result(n_row_out, n_col_out);
   for (int j = 0; j < n_col_out; ++j) {
     // Calculate result[, j] = A %*% B[, j] in column-oriented manner for each j
     // Fill in
+    NumericVector b = B(Rcpp::_, j);
+    for (int i = 0; i < n_row_A; ++i){
+      for (int q = 0; q < n_row_out; ++q){
+        result(q, j) += A(q, i) * b[i];
+      }
+    }
   }
   return result;
 }
